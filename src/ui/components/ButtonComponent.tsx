1c0c30d9799a15e349d58c7122f2bfe3cf91a51a
divyanshchahar <divyanshchahar@outlook.com>
2026-05-12 15:18:03 +0530

    update: added click handler

--- src/ui/components/ButtonComponent.tsx
+++ src/ui/components/ButtonComponent.tsx
@@ -5,12 +5,14 @@ type ButtonComponentPropTypes = {
     version: "cta" | "basic"
     state: "ready" | "disabled" | "loading"
     colorScheme: string
+    clickHandler: () => void
 };
 
-export const ButtonComponent = ({children, version, state, colorScheme}: ButtonComponentPropTypes) => {
+export const ButtonComponent = ({children, version, state, colorScheme, clickHandler}: ButtonComponentPropTypes) => {
     return (
         <button disabled={state === "disabled"}
-                className={`${styles.common} ${version === "cta" && styles.cta} ${version === "basic" && styles.basic} ${"boldN"} ${colorScheme} ${state === "disabled" && styles.disabled}`}>
+                className={`${styles.common} ${version === "cta" && styles.cta} ${version === "basic" && styles.basic} ${"boldN"} ${colorScheme} ${state === "disabled" && styles.disabled}`}
+                onClick={clickHandler}>
             {children}
         </button>
     );
